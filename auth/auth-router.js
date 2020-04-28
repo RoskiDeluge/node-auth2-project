c2227aea35c0a234f22980eeec328d4d115e34eb
Roberto Delgado <roberto.delgado@gmail.com>
2020-04-27 14:58:24 -1000

    Small refactor of auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,12 +7,6 @@ const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
 
-//----------------------------------------------------------------------------//
-// We didn't do this in class, but it makes sense to return a token when someone
-// registers... why make them log in when they just barely provided a new
-// username and password? Just return them a token, so they can begin using it
-// right away... 
-//----------------------------------------------------------------------------//
 router.post('/register', (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
@@ -20,9 +14,7 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      // pass the created user into the genToken() method, and get the token
       const token = genToken(saved);
-      // return the user object, and the token.
       res.status(201).json({ created_user: saved, token: token });
     })
     .catch(error => {
@@ -30,10 +22,7 @@ router.post('/register', (req, res) => {
     });
 });
 
-//----------------------------------------------------------------------------//
-// When someone successfully authenticates, reward them with a token, so they
-// don't have to authenticate again. 
-//----------------------------------------------------------------------------//
+
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
@@ -41,9 +30,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        // pass the found user into the genToken() method, and get the token
+
         const token = genToken(user);
-        // return the found user's username, and the token"
+
         res.status(200).json({ username: user.username, token: token });
       } else {
         res.status(401).json({ message: 'You shall not pass!' });
@@ -54,14 +43,9 @@ router.post('/login', (req, res) => {
     });
 });
 
-//----------------------------------------------------------------------------//
-// This is a helper method that helps us stay DRY (we generate tokens in both
-// the POST /api/auth/register handler, and the POST /api/auth/login handler).
-// Uses the same pattern as our test handler for GET /token in server.js. 
-//----------------------------------------------------------------------------//
+
 function genToken(user) {
 
-  // create the payload...
   const payload = {
     userid: user.id,
     username: user.username,
